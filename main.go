7341ef59ca97985c71a63b1fc964ca4ef4105310
arglp <leopold.prueller@gmx.at>
2025-05-30 18:47:24 +0200

    added simple repl

--- main.go
+++ main.go
@@ -3,10 +3,22 @@ package main
 import (
 	"fmt"
 	"strings"
+	"bufio"
+	"os"
 )
 
 func main() {
-	fmt.Println(cleanInput("hello world"))
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		ok := scanner.Scan()
+		if !ok {
+			fmt.Println("An error occured")
+		}
+		userInput := scanner.Text()
+		cleanedUserInput := cleanInput(userInput)
+		fmt.Printf("Your command was: %s\n", cleanedUserInput[0])
+	}
 }
 
 func cleanInput(text string) []string {
