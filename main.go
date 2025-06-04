329c0e6c26cb8010113c478afc582c95ad5797bb
arglp <leopold.prueller@gmx.at>
2025-06-04 13:18:53 +0200

    Started from scratch

--- main.go
+++ main.go
@@ -2,26 +2,8 @@ package main
 
 import (
 	"fmt"
-	"strings"
-	"bufio"
-	"os"
 )
 
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		ok := scanner.Scan()
-		if !ok {
-			fmt.Println("An error occured")
-		}
-		userInput := scanner.Text()
-		cleanedUserInput := cleanInput(userInput)
-		fmt.Printf("Your command was: %s\n", cleanedUserInput[0])
-	}
-}
-
-func cleanInput(text string) []string {
-	words := strings.Fields(strings.ToLower(strings.TrimSpace(text)))
-	return words
+	fmt.Println("Hello, World!")
 }
